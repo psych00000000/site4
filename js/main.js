d4a9e739457cf751031755a3e385948dcec37d46
psych00000000 <andryha.h.229@gmail.com>
2024-05-06 02:17:16 +0500

    comm1t

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$ (document).ready(function() {
+    $('nav a[href^="#"]').click(function() {
+        let target  = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        },500);
+        $('nav a[href^="#"]').parent().removeClass('active');
+        $(this).parent().addClass('active');
+        $('.menu').toggle(500);
+        return false;
+    })
+    $('.menu_burger').click(function(){
+        $('.menu').toggle(500);
+    })
+});
